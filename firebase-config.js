46cc3424b135d29be571d39d8e5084c366202747
ladoucedx-png <ladoucedx@gmail.com>
2026-05-25 01:34:17 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 window.firebaseConfig = {
-  apiKey: "TA_CLE_API",
-  authDomain: "overstate-rp.firebaseapp.com",
-  databaseURL: "https://overstate-rp-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "overstate-rp",
-  storageBucket: "overstate-rp.appspot.com",
-  messagingSenderId: "TON_MESSAGING_ID",
-  appId: "TON_APP_ID"
+  apiKey: "AIzaSyAD1Oq-ius4xTK7ROZMWjCHOtlAx3U4NiA",
+  authDomain: "site-overstate.firebaseapp.com",
+  databaseURL: "https://site-overstate-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "site-overstate",
+  storageBucket: "site-overstate.firebasestorage.app",
+  messagingSenderId: "479042335097",
+  appId: "1:479042335097:web:ca088f7648069be5a5bfa8"
 };
