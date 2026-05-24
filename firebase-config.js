e2505c81e2cdc8069b95423fcb273c559d4691c3
ladoucedx-png <ladoucedx@gmail.com>
2026-05-25 01:24:02 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// COLLE ICI LA CONFIG FIREBASE DE TON PROJET
+// Firebase Console > Project settings > Your apps > Web app > Firebase SDK config
+window.firebaseConfig = {
+  apiKey: "REMPLACE_MOI",
+  authDomain: "REMPLACE_MOI.firebaseapp.com",
+  databaseURL: "https://REMPLACE_MOI-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "REMPLACE_MOI",
+  storageBucket: "REMPLACE_MOI.appspot.com",
+  messagingSenderId: "REMPLACE_MOI",
+  appId: "REMPLACE_MOI"
+};
