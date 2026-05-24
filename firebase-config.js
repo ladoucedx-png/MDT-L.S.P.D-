59345822444a91a80d3c26c478bc81205b719c05
ladoucedx-png <ladoucedx@gmail.com>
2026-05-25 01:29:58 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,9 @@
-// COLLE ICI LA CONFIG FIREBASE DE TON PROJET
-// Firebase Console > Project settings > Your apps > Web app > Firebase SDK config
 window.firebaseConfig = {
-  apiKey: "REMPLACE_MOI",
-  authDomain: "REMPLACE_MOI.firebaseapp.com",
-  databaseURL: "https://REMPLACE_MOI-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "REMPLACE_MOI",
-  storageBucket: "REMPLACE_MOI.appspot.com",
-  messagingSenderId: "REMPLACE_MOI",
-  appId: "REMPLACE_MOI"
+  apiKey: "TA_CLE_API",
+  authDomain: "overstate-rp.firebaseapp.com",
+  databaseURL: "https://overstate-rp-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "overstate-rp",
+  storageBucket: "overstate-rp.appspot.com",
+  messagingSenderId: "TON_MESSAGING_ID",
+  appId: "TON_APP_ID"
 };
